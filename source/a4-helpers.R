4bd79d0e9c968f172b3ac8aea89f055692c60b32
Yetong Gu <yg84@uw.edu>
2022-11-22 16:53:30 -0800

    I will be making changes here.

--- source/a4-helpers.R
+++ source/a4-helpers.R
@@ -5,8 +5,8 @@ library(tidyverse)
 # This function returns the incarceration data as a dataframe
 # Note: The CSV file is stored on my local machine to speed load times
 #---------------------------------------------------------------------------#
-get_data <- function(num_records=-1) {
-  fname <- "~/Documents/info201/data/incarceration_trends.csv"
+get_data <- function(num_records) {
+  fname <- "incarceration_trends.csv"
   df <- read.csv(fname, nrows=num_records)
   return(df)
 }
--- source/analysis.R
+++ source/analysis.R
@@ -1,26 +1,29 @@
 library(tidyverse)
-
+library(dplyr)
+library(ggplot2)
 # The functions might be useful for A4
 source("../source/a4-helpers.R")
 
-## Test queries ----
-#----------------------------------------------------------------------------#
-# Simple queries for basic testing
-#----------------------------------------------------------------------------#
-# Return a simple string
-test_query1 <- function() {
-  return ("Hello world")
-}
-
-# Return a vector of numbers
-test_query2 <- function(num=6) {
-  v <- seq(1:num)
-  return(v)
-}
 
 ## Section 2  ---- 
 #----------------------------------------------------------------------------#
 # Your functions and variables might go here ... <todo: update comment>
+
+data <- read.csv("incarceration_trends.csv", nrows = 10000, stringsAsFactors = FALSE)
+
+jailAverage <- data %>%
+  filter(year == 2002) %>%
+  summarize(averageNum = mean(total_jail_pop, na.rm = TRUE)) %>%
+  pull(averageNum)
+
+blackPrisonAverage <- data %>%
+  summarize(averageValue = mean(black_prison_pop, na.rm = TRUE)) %>%
+  pull(averageValue)
+whitePrisonAverage <- data %>%
+  summarize(averageValue = mean(white_prison_pop, na.rm = TRUE)) %>%
+  pull(averageValue)
+
+
 #----------------------------------------------------------------------------#
 
 ## Section 3  ---- 
@@ -28,24 +31,67 @@ test_query2 <- function(num=6) {
 # Growth of the U.S. Prison Population
 # Your functions might go here ... <todo:  update comment>
 #----------------------------------------------------------------------------#
+
 # This function ... <todo:  update comment>
+# This function produce a bar chart shows the growth of the U.S. prison population from 1970 to 2018
 get_year_jail_pop <- function() {
-  # TODO: Implement this function 
-return()   
+  # TODO: Implement this function
+  total_jail_pop_year <- data %>%
+    group_by(year) %>%
+    summarise(total_jail_pop = sum(total_jail_pop, na.rm = TRUE))
+  return(total_jail_pop_year)
 }
 
-# This function ... <todo:  update comment>
-plot_jail_pop_for_us <- function()  {
-  # TODO: Implement this function 
-  return()   
-} 
-
+# This function produce a bar chart shows the growth of the U.S. prison population from 1970 to 2018
+plot_jail_pop_for_us <- function() {
+  # TODO: Implement this function
+  total_jail_pop_chart <- get_year_jail_pop() %>%
+    ggplot(total_jail_pop_year, mapping = aes(x = year, y = total_jail_pop)) +
+    geom_col() +
+    labs(
+      title = "Increase of Jail Population in U.S. (1970-2018)",
+      x = "Year",
+      y = "Total Jail Population"
+    )
+  return(total_jail_pop_chart)   
+}
+plot_jail_pop_for_us()
 ## Section 4  ---- 
 #----------------------------------------------------------------------------#
 # Growth of Prison Population by State 
 # Your functions might go here ... <todo:  update comment>
 # See Canvas
 #----------------------------------------------------------------------------#
+
+get_year_jail_pop_states <- function(states) {
+   growth_by_state <- data %>%
+     filter(state %in% states) %>%
+     group_by(state, year) %>%
+     summarise(year_jail_pop = sum(total_pop,na.rm = TRUE) )
+   return(growth_by_state)
+  }
+
+state_vector <- c("AL", "CA", "AR")
+plot_jail_pop_for_states <- function(states) {
+jail_pop_chart <- ggplot(get_year_jail_pop_states(states)) + 
+geom_line(mapping = aes(x =year, y=year_jail_pop, color = state)) + 
+labs(x = "Year", y = "jail Population Per Year", title = "Yearly Growth of Prison
+       Population", caption = "Yearly Growth of Prison Population from 1970-2018 in
+       the Selected States")
+return(jail_pop_chart)
+  
+  
+}
+
+plot_jail_pop_for_states(state_vector)
+
+
+
+
+
+
+
+
 
 ## Section 5  ---- 
 #----------------------------------------------------------------------------#
@@ -54,12 +100,62 @@ plot_jail_pop_for_us <- function()  {
 # See Canvas
 #----------------------------------------------------------------------------#
 
+get_female_pop_by_state <- function() {
+  female_data <- data %>% 
+    group_by(year, state) %>% 
+    summarise(female_pop = sum(female_jail_pop, na.rm = TRUE))
+  return(female_data)
+}
+
+plot_female_pop_by_state <- function() {
+  chart3 <- ggplot(data = get_female_pop_by_state(), 
+                   aes(x = year, y = female_pop, group = state)) +
+    geom_line(aes(color = state)) +
+    xlim(1970, 2018) +
+    xlab("Year") +
+    ylab("Female Incarcerated Population") +
+    ggtitle("Female Incarcerated Population by State (1970-2018)")
+  
+  return(chart3)
+}
+
+plot_female_pop_by_state()
+
+
+
+
 ## Section 6  ---- 
 #----------------------------------------------------------------------------#
 # <a map shows potential patterns of inequality that vary geographically>
 # Your functions might go here ... <todo:  update comment>
 # See Canvas
 #----------------------------------------------------------------------------#
+
+map_df <- function() {
+ df <- data %>%
+ filter(year == 2018) %>%
+   group_by(state) %>%
+   summarize(prop_black_2018 = sum(black_pop_15to64, na.rm = TRUE)/ sum(total_pop, na.rm = TRUE))
+ return(df)
+}
+
+map_df <- map_df()
+state_shape <- map_data("state")
+1map_chart <-function() {
+  p <- ggplot(state_shape) +
+    geom_polygon(
+      mapping = aes(x = long, y = lat, group = group),
+      fill = prop_black_2018,
+      color = "white",
+      size = 0.1
+      
+  )
+  
+  return(p)
+  }
+
+map_chart <- map_chart()
+
 
 ## Load data frame ---- 
 
